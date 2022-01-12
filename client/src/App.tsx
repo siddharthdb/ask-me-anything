79fedc3bdf63e817c010b3d5928b52bd2cf6cd9b
Siddharth Bhawalkar <siddharthdb@gmail.com>
2022-01-12 09:40:06 -0500

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Main from "./pages/main";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
